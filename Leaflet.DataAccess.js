b2964d633beb36e9e9f13b83820db577120b48c2
DannyB138 <113048379+DannyB138@users.noreply.github.com>
2023-03-29 11:15:05 -0500

    DataAccessRefactor - Complete Visual Elements

--- Leaflet.DataAccess.js
+++ Leaflet.DataAccess.js
@@ -1,4 +1,4 @@
-function createMockData(yearStart = 1800, yearEnd = 2020, widthMin = 0.05, widthMax = 5) {
+function createMockData1(yearStart = 1800, yearEnd = 2020, widthMin = 0.05, widthMax = 5) {
     let jsonOut = [];
 
     let nrows = yearEnd - yearStart + 1;
@@ -18,8 +18,61 @@ function createMockData(yearStart = 1800, yearEnd = 2020, widthMin = 0.05, width
     return jsonOut;
 }
 
+function createMockData(yearStart = 1800, yearEnd = 2020, widthMin = 0.05, widthMax = 50) {
+    let x = [];
+    let yTW = [];
+    let yEW = [];
+    let yLW = [];
+
+    let nrows = yearEnd - yearStart + 1;
+    for (i = 0; i < nrows; i++) {
+        let width = Math.random() * (widthMax - widthMin) + widthMin;
+
+        x.push(yearStart + i);
+        yTW.push(width);
+        yEW.push(width * (1/3));
+        yLW.push(width * (2/3));
+    }
+
+    let jsonTW = {
+        "x": x,
+        "y": yTW,
+    }
+    let jsonEW = {
+        "x": x,
+        "y": yEW,
+    }
+    let jsonLW = {
+        "x": x,
+        "y": yLW,
+    }
+
+    return {
+        "tw": jsonTW,
+        "ew": jsonEW,
+        "lw": jsonLW
+    };
+}
+
+// Handlebars.registerHelper('decimalFixed', function(decimal) {
+//     if(decimal){
+//         return Math.round(decimal * 1000) / 1000;
+//     }
+//     return "";
+//     });
+Handlebars.registerHelper('decimalFixed', function(decimal) {
+    // console.log(decimal, typeof(decimal));
+    if(decimal){
+        decimal = decimal.toString()
+        var dec_idx = decimal.indexOf('.');
+        var rounded = decimal.slice(0,dec_idx+4);
+        return rounded;
+    }
+    return typeof(decimal);
+});
+
 var initialData = createMockData()
-console.log(initialData);
+// console.log(initialData);
 var element = document.getElementById("DataAccess-Table-ID")
 var html = element.innerHTML;
 var tableTemplate = Handlebars.compile(html);
