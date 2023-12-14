0567a14975f9c2a1b086af1812854027b64a60de
thorn573 <68254973+thorn573@users.noreply.github.com>
2023-12-14 10:53:05 -0600

    Created file for pith estimate functions.

---
+++ Leaflet.PithEstmate.js
@@ -0,0 +1,15 @@
+/**
+ * @file Leaflet Pith Estamte
+ * @author Jessica Thorne <thorn572@umn.edu>
+ * @version 1.0.0
+ */
+
+/**
+ * Interface for pith estimate tools. 
+ * @constructor
+ * 
+ * @param {object} Lt - LTreering object from leaflet-treering.js. 
+ */
+function PithEstimateInterface(Lt) {
+    this.treering = Lt;
+}
